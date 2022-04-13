27aa0137e2daa73282b15421f905f58fbfa94b9b
OlegTch <81326823+OlegTch@users.noreply.github.com>
2022-04-13 12:12:05 +0300

    add subscription

--- controllers/users/updateSubscriptionUser.js
+++ controllers/users/updateSubscriptionUser.js
@@ -3,23 +3,19 @@ const { HTTP_STATUS_CODE } = require('../../libs');
 
 const updateSubscriptionUser = async (req, res, next) => {
   const { email, subscription } = req.user;
-
   const body = req.body;
   const { userId } = req.params;
-  console.log('subscription: ', subscription);
-  console.log('req.params: ', req.params);
 
-  const user = await User.findOneAndUpdate(
-    { _id: userId, owner: _id },
-    { ...body },
-    { subscription }
-  );
+  console.log('req.body: ', req.body);
+
+  const user = await User.findOneAndUpdate({ _id: userId }, { ...body }, { new: true });
 
   if (!user) {
     return res
       .status(HTTP_STATUS_CODE.NOT_FOUND)
       .json({ status: 'error', code: HTTP_STATUS_CODE.NOT_FOUND, message: 'Not found' });
   }
+
   return res.json({
     status: 'success',
     code: HTTP_STATUS_CODE.OK,
@@ -31,19 +27,5 @@ const updateSubscriptionUser = async (req, res, next) => {
     },
   });
 };
-
-// const getCurrent = async (req, res) => {
-//   const { email, subscription } = req.user;
-//   res.json({
-//     status: 'succes',
-//     code: HTTP_STATUS_CODE.OK,
-//     ResponseBody: {
-//       user: {
-//         email,
-//         subscription,
-//       },
-//     },
-//   });
-// };
 
 module.exports = updateSubscriptionUser;
